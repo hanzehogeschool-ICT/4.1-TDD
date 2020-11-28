e35cc358e27a62d8b2290e16e808d5f4843bd6b0
timostrating <timostrating@home.nl>
2020-11-28 13:39:48 +0100

    added the general function names

---
+++ FakeGradingTest.java
@@ -0,0 +1,37 @@
+package nl.hanze.hive.fakegradingtest;
+
+import org.junit.jupiter.api.Test;
+
+public class FakeGradingTest {
+
+    public void testSlideThroughSpace() {}
+    public void testSpiderSlide() {}
+    public void testSpiderStack() {}
+    public void testAntMove() {}
+    public void testPlayerWin() {}
+    public void testDraw() {}
+    public void testPass() {}
+    public void testMoveWithoutBee() {}
+    public void testGrasshopperJumpSpace() {}
+    public void testBeetleSlide() {}
+    public void testBeetleStack() {}
+    public void testMoveOpposingTiles() {}
+    public void testTurnFourBee() {}
+    public void testPassOnlyClimb() {}
+    public void testSlideThroughGate() {}
+    public void testAntSlide() {}
+    public void testAntStack() {}
+    public void testSpiderRevisitHexagon() {}
+    public void testClimbThroughStackedGate() {}
+    public void testPlayerWinCovered() {}
+    public void testClimbThroughGate() {}
+    public void testMoveTilesAdjacent() {}
+    public void testInitialTiles() {}
+    public void testHandSize() {}
+    public void testGrasshopperJump() {}
+    public void testGrasshopperMove() {}
+    public void testSplitHive() {}
+    public void testPlayStacked() {}
+    public void testBeeSlide() {}
+    public void testBeeStack() {}
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 4.1-TDD
+
+Add the fakegradingtest to your test folder using 
+`git submodule add https://github.com/hanzehogeschool-ICT/4.1-TDD {{PATH}}`
+
+Where {{PATH}} is probable `src/test/java/nl/hanze/hive/frakegradingtest`
